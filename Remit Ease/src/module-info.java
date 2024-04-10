f7f29ff13896b733c91152670084231b02dd4378
jrgdnn12 <145727847+jrgdnn12@users.noreply.github.com>
2024-04-10 02:32:53 +0000

    Starting Fresh. Decluter test files.

--- Remit Ease/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Remit Ease {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- tsrdg/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module tsrdg {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
