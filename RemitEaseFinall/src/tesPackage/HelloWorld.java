5f4754183c36cc8afa3ff5f0c6f93f453ccefaaa
jrgdnn12 <145727847+jrgdnn12@users.noreply.github.com>
2024-04-10 03:40:02 +0000

    coomit

--- RemitEaseFinall/src/tesPackage/HelloWorld.java
+++ RemitEaseFinall/src/tesPackage/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		System.out.println("RemitEase is Global Remittance System Application built in javaFX.");
 		System.out.println("This Project Will Fullfill the requirements for MIS 768 Software Concepts");
 		System.out.println("Authors: Jorge Alvarez & Phillip Mensa");
+		System.out.println("doing sum testing")''
 		
 	}
 
